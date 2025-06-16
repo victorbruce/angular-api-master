734cbb6c6d0d1131e7ed5ada2319539b034faab4
Victor Bruce <victorbruce82@gmail.com>
2025-06-16 10:07:44 +0000

    feat: route to the posts page from the root app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-api-master';
