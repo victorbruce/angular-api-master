bb059a3bc078c004f7f04b1540e32b922b700aa9
Victor Bruce <victorbruce82@gmail.com>
2025-06-16 18:07:45 +0000

    feat: implement a component to display a single post and its comments

---
+++ src/app/pages/post-details/post-details.component.ts
@@ -0,0 +1,63 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiClientService } from '../../services/api-client.service';
+import { CommonModule } from '@angular/common';
+import { Post, Comment } from '../../models/';
+import { API_BASE_URL } from '../../shared/constants';
+
+@Component({
+  selector: 'app-post-details',
+  imports: [CommonModule],
+  templateUrl: './post-details.component.html',
+  styleUrl: './post-details.component.scss',
+})
+export class PostDetailsComponent implements OnInit {
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  post: Post | null = null;
+  comments: Comment[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor(private apiClient: ApiClientService) {}
+
+  postId: string | null = null;
+
+  ngOnInit(): void {
+    this.postId = this.route.snapshot.paramMap.get('id');
+
+    if (this.postId) {
+      this.fetchPostAndComments();
+    }
+  }
+
+  fetchPostAndComments(): void {
+    this.loading = true;
+    this.error = null;
+
+    this.apiClient.get<Post>(`${API_BASE_URL}/posts/${this.postId}`).subscribe({
+      next: (data) => {
+        this.post = data;
+        this.fetchComments();
+      },
+      error: (err) => {
+        this.error = 'Failed to load post';
+        this.loading = false;
+      },
+    });
+  }
+
+  fetchComments(): void {
+    this.apiClient
+      .get<Comment[]>(`${API_BASE_URL}/posts/${this.postId}/comments`)
+      .subscribe({
+        next: (data) => {
+          this.comments = data;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.error = 'Failed to load comments';
+          this.loading = false;
+        },
+      });
+  }
+}
