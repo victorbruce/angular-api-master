b97ab1f5baafd99bb8e05ba179ecf401ebcf84ee
Victor Bruce <victorbruce82@gmail.com>
2025-06-16 18:05:06 +0000

    feat: link to the post details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { PostsComponent } from './pages/posts/posts.component';
+import { PostDetailsComponent } from './pages/post-details/post-details.component';
 
 export const routes: Routes = [
-	{
-		path: '',
-		component: PostsComponent,
-		title: 'Posts',
-	}
+  {
+    path: '',
+    component: PostsComponent,
+    title: 'Posts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'posts/:id',
+    component: PostDetailsComponent,
+    title: 'Post Details',
+  },
 ];
