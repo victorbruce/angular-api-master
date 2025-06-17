5ab279ca8c41ecef20314382f76f6303e089e667
Victor Bruce <victorbruce82@gmail.com>
2025-06-17 16:54:09 +0000

    refactor: add new post route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PostsComponent } from './pages/posts/posts.component';
 import { PostDetailsComponent } from './pages/post-details/post-details.component';
+import { NewPostComponent } from './pages/new-post/new-post.component';
 
 export const routes: Routes = [
   {
@@ -8,6 +9,11 @@ export const routes: Routes = [
     component: PostsComponent,
     title: 'Posts',
     pathMatch: 'full',
+  },
+  {
+    path: 'posts/new',
+    component: NewPostComponent,
+    title: 'New Post',
   },
   {
     path: 'posts/:id',
