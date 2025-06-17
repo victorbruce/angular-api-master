80e81d893297bd11a00d8b1448d3c9d450e92317
Victor Bruce <victorbruce82@gmail.com>
2025-06-17 17:50:02 +0000

    feat: create a new post service to hold posts fetched from API and simulate adding new post

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Post } from '../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  private postsSubject = new BehaviorSubject<Post[]>([]);
+  posts$ = this.postsSubject.asObservable();
+
+  // call this when fetching from API
+  setPosts(posts: Post[]): void {
+    this.postsSubject.next(posts);
+  }
+
+  addPost(post: Post): void {
+    const currentPosts = this.postsSubject.value;
+    this.postsSubject.next([post, ...currentPosts]);
+  }
+
+  getPosts(): Post[] {
+    return this.postsSubject.value;
+  }
+}
