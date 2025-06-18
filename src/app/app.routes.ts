682e2b754a2e08400934d14a7e45e82f862d0e3d
Victor Bruce <victorbruce82@gmail.com>
2025-06-18 09:02:15 +0000

    feat: add an edit button to navigate to the edit form post page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PostsComponent } from './pages/posts/posts.component';
 import { PostDetailsComponent } from './pages/post-details/post-details.component';
 import { NewPostComponent } from './pages/new-post/new-post.component';
+import { EditPostComponent } from './pages/edit-post/edit-post.component';
 
 export const routes: Routes = [
   {
@@ -19,5 +20,10 @@ export const routes: Routes = [
     path: 'posts/:id',
     component: PostDetailsComponent,
     title: 'Post Details',
+  },
+  {
+    path: 'posts/:id/edit',
+    component: EditPostComponent,
+    title: 'Edit Post',
   },
 ];
